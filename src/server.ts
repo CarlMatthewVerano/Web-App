9078608c90376b1b2727bb76edaf7062b59e13bf
CarlMatthewVerano <carlmatthewverano@gmail.com>
2025-11-05 10:28:42 +0800

    chore: some package installations including typedi (dependency injection), bcrypt, etc.

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
+import "reflect-metadata";
 // const express = require("express");
+
+import { ValidateEnv } from "#utils/validateEnv.js";
+import { App } from "#app/app.js";
+import { routes } from "#src/routes.js";
+
+ValidateEnv();
+
+const app = new App(routes);
+app.listen();
